caadbd09ce0eec4b1d1fc80eaec3c61ccfed6d90
Matthew Okadinya <matthewokadinya@gmail.com>
2023-10-21 22:30:34 +0100

    practice counter done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
+  const counter = useSelector((state)=>state.counter)
+  const dispatch = useDispatch()
+  const increment = () => {
+    dispatch({type:'INC'})
+  }
+  const decrement = () => {
+    dispatch({type:'DEC'})
+  }
+  const addValue = () => {
+    dispatch({type:'ADD', payload: 10})
+  }
   return (
-    <div>App</div>
+   <div>
+    <h1>Counter app</h1>
+    <h2>{counter}</h2>
+    <button onClick={decrement}>-</button>
+    <button onClick={increment}>+</button>
+    <button onClick={addValue}>add</button>
+   </div>
   )
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,18 @@
-import { createStorehook } from "react-redux";
+import { createStore } from "redux";
 
-const reducer = (state={counter: 0}, action)=>{
+const reducerFn = (state={counter: 0}, action)=>{
 
+    if(action.type === 'INC'){
+        return {counter:state.counter  + 1}
+    }
+    if(action.type === 'DEC'){
+        return{counter:state.counter - 1}
+    }
+    if(action.type === 'ADD'){
+        return{counter:state.counter + action.payload}
+    }
+    return state
 }
 
-const store =  createStore(reducer)
+const store =  createStore(reducerFn)
 export default store

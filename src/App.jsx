f21eee91fee7c1dca475fa57fb91de02bd63247c
Matthew Okadinya <matthewokadinya@gmail.com>
2023-10-21 22:48:13 +0100

    using react tool kit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { actions } from './store'
 
 const App = () => {
   const counter = useSelector((state)=>state.counter)
   const dispatch = useDispatch()
   const increment = () => {
-    dispatch({type:'INC'})
+    dispatch(actions.increment())
   }
   const decrement = () => {
-    dispatch({type:'DEC'})
+    dispatch(actions.decrement())
   }
   const addValue = () => {
-    dispatch({type:'ADD', payload: 10})
+    dispatch(actions.addBy(10))
   }
   return (
    <div>

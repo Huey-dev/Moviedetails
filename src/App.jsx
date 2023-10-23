55ead0e364172b22569c37b17ae1dca7324c7d10
Matthew Okadinya <matthewokadinya@gmail.com>
2023-10-23 23:08:49 +0100

    Pages routed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import {
+  BrowserRouter as BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="App">
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route index element={<Home/>} />
+          <Route path="/movie/:imdbID" element={<MovieDetail/>} />
+          <Route path="*" element={<PageNotFound/>} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
